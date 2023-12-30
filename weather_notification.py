550b85171db99ccf190c003d04d15b1d63eaa3bb
MANISH MISHRA <manishkr.mishra27d@gmail.com>
2023-12-30 17:11:17 +0530

    python based app which show current weather and temperature in delhi.

---
+++ weather_notification.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+#from win10toast import ToastNotifier
+
+def getdata(url):
+    r = requests.get(url)
+    return r.text
+
+htmldata =getdata("https://weather.com/en-IN/weather/tenday/l/Rajpath+Area+Delhi?canonicalCityId=bd07c6679ad5069265f742eb66d5d53172210b02b127807d6a47e92807799fbe")
+
+soup = BeautifulSoup(htmldata, 'html.parser')
+
+ 
+chances_wather = soup.find_all("p", class_="DailyContent--narrative--3Ti6_")
+
+ 
+  
+temp_weather = str(chances_wather) 
+
+result ="\n"+"current weather status in Delhi: " + "\n\n" + temp_weather[66:153] +"\n\n" +"temperature: " + temp_weather[154:162]
+
+
+print(result)
